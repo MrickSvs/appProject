d7c4e9b9156945c6bc1a29f808ddc9159d07fda6
Rikem <aymeric.saves@gmail.com>
2023-09-03 21:33:31 +0200

    modale ok

--- mobile_app/lib/screens/home_screen.dart
+++ mobile_app/lib/screens/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:mobile_app/services/api_service.dart'; // Assurez-vous de remplacer par le chemin correct vers api_service.dart
+import 'package:mobile_app/services/api_service.dart';
+import 'package:mobile_app/widgets/shop_details_modal.dart'; // Assurez-vous que le chemin est correct
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,7 +10,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final apiService = ApiService(baseUrl: 'http://localhost:3000'); // Remplacez par l'URL de votre backend
+  final apiService = ApiService(baseUrl: 'http://localhost:3000');
+
+  bool isModalOpen = false; // État pour contrôler l'affichage de la modale
+  late dynamic selectedShop; // Stocke les détails du shop sélectionné
 
   @override
   Widget build(BuildContext context) {
@@ -21,29 +25,75 @@ class _HomeScreenState extends State<HomeScreen> {
         future: apiService.fetchShops(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator()); // Affiche un indicateur de chargement pendant le chargement des données.
+            return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
-            // Convertit les données en marqueurs pour la carte.
             final markers = snapshot.data!.map((shop) {
               return Marker(
                 point: LatLng(shop['gps']['coordinates'][0], shop['gps']['coordinates'][1]),
-                builder: (ctx) => Icon(Icons.location_on, color: Colors.red),
+                builder: (ctx) => GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isModalOpen = true;
+                      selectedShop = shop;
+                    });
+                  },
+                  child: Icon(Icons.location_on, color: Colors.red),
+                ),
               );
             }).toList();
 
-            return FlutterMap(
-              options: MapOptions(
-                center: LatLng(48.8566, 2.3522), // Coordonnées de Paris
-                zoom: 13.0,
-              ),
-              layers: [
-                TileLayerOptions(
-                  urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: ['a', 'b', 'c'],
+            return Stack(
+              children: [
+                FlutterMap(
+                  options: MapOptions(
+                    center: LatLng(48.8566, 2.3522),
+                    zoom: 13.0,
+                  ),
+                  layers: [
+                    TileLayerOptions(
+                      urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: ['a', 'b', 'c'],
+                    ),
+                    MarkerLayerOptions(markers: markers),
+                  ],
                 ),
-                MarkerLayerOptions(markers: markers),
+                // Modale
+                if (isModalOpen)
+                  Positioned.fill(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isModalOpen = false;
+                        });
+                      },
+                      child: Container(
+                        color: Colors.black.withOpacity(0.5),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Stack(
+                                alignment: Alignment.topRight,
+                                children: [
+                                  ShopDetailsModal(shopDetails: selectedShop),
+                                  IconButton(
+                                    icon: Icon(Icons.close),
+                                    onPressed: () {
+                                      setState(() {
+                                        isModalOpen = false;
+                                      });
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
               ],
             );
           }
--- server/routes/shops.js
+++ server/routes/shops.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const Shop = require('../models/shops'); // Assurez-vous que le chemin est correct
+const express = require("express");
+const router = new express.Router();
+const Shop = require("../models/shops");
 
-// Endpoint pour récupérer tous les shops
-router.get('/', async (req, res) => {
-    try {
-        const shops = await Shop.find();
-        res.json(shops);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+router.get("/", async (req, res) => {
+  try {
+    const shops = await Shop.find();
+    res.json(shops);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
 });
 
 module.exports = router;
