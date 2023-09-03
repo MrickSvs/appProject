ab23382302980d2f5bc04d5ef4d00fb134b568ff
Rikem <aymeric.saves@gmail.com>
2023-09-03 14:21:09 +0200

    fix url localhost

--- mobile_app/lib/services/api_service.dart
+++ mobile_app/lib/services/api_service.dart
@@ -7,7 +7,7 @@ class ApiService {
   ApiService({required this.baseUrl});
 
   Future<List<dynamic>> fetchShops() async {
-    final response = await http.get(Uri.parse('$baseUrl/shops')); // Remplacez '/shops' par l'endpoint approprié si différent.
+    final response = await http.get(Uri.parse('http://192.168.1.11:3000/shops'));
 
     if (response.statusCode == 200) {
       return json.decode(response.body);
